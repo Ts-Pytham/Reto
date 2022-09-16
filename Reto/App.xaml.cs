52779a7dc8f385a1ee9110844afc863fbc9e84e3
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-09-16 12:31:38 -0500

    Agregar archivos de proyecto.

---
+++ Reto/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Reto;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Reto/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Reto;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
